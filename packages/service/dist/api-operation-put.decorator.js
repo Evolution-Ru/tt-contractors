1d1322df6047909645acd43caa033371f613f9d5
y.chukaev <chukaev@live.ru>
2018-12-25 22:26:14 +0300

    remove dist

--- packages/service/dist/api-operation-put.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var swagger_service_1 = require("./swagger.service");
-function ApiOperationPut(args) {
-    return function (target, propertyKey, descriptor) {
-        swagger_service_1.SwaggerService.getInstance().addOperationPut(args, target, propertyKey);
-    };
-}
-exports.ApiOperationPut = ApiOperationPut;
--- packages/service/dist/api-path.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var swagger_service_1 = require("./swagger.service");
-function ApiPath(args) {
-    return function (target) {
-        swagger_service_1.SwaggerService.getInstance().addPath(args, target);
-    };
-}
-exports.ApiPath = ApiPath;
